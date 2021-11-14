0eacabb2132f6f1d174b7e8e07c8ff734a5e01ff
Puwiii <Sebastian.mathieur@gmail.com>
2021-11-14 14:23:02 -0300

    add remove button and update time ago

--- src/components/Transaction/Transaction.js
+++ src/components/Transaction/Transaction.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Price from "../Atoms/Price";
 import Cryptocurrency from "../Cryptocurrency/Cryptocurrency";
 import "moment/locale/es";
 import moment from "moment";
+
+import {
+  AiOutlineCalendar,
+  AiOutlineDollarCircle,
+  AiOutlineCheckCircle,
+  AiOutlineClockCircle,
+  AiOutlineDelete,
+} from "react-icons/ai";
 
 import {
   TransactionContainer,
@@ -11,18 +19,40 @@ import {
   CoinContainer,
   InfoField,
   Moment,
+  TransactionRemove,
 } from "./Transaction.elements";
 
 import { OPERATION_TYPE } from "../AddTransactionForm/AddTransactionForm";
+import { useDispatch } from "react-redux";
+import { removeTransaction } from "../../reducers/transactionReducer";
 
 const Transaction = ({ id, coin, type, amount, price, date }) => {
   moment.locale("es");
 
   const formattedDate = moment(date).format("l");
+  const [timeAgo, setTimeAgo] = useState(null);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setTimeAgo(moment(date).startOf().fromNow());
+    setInterval(() => {
+      setTimeAgo(moment(date).startOf().fromNow());
+    }, [30000]);
+  }, [date]);
+
+  const remove = () => {
+    dispatch(removeTransaction(id));
+  };
 
   return (
     <TransactionContainer>
-      <Moment>{moment(date).startOf().fromNow()}</Moment>
+      {timeAgo && (
+        <Moment>
+          <AiOutlineClockCircle />
+          {timeAgo}
+        </Moment>
+      )}
       <TransactionType>
         {type === OPERATION_TYPE.BUY && "Compra"}
         {type === OPERATION_TYPE.SELL && "Venta"}
@@ -40,15 +70,31 @@ const Transaction = ({ id, coin, type, amount, price, date }) => {
           <span>Cantidad</span> {amount}
         </InfoField>
         <InfoField>
-          <span>Precio</span> <Price price={price} operationType={type} />
+          <span>
+            <AiOutlineDollarCircle />
+            Precio
+          </span>{" "}
+          <Price price={price} operationType={type} />
         </InfoField>
         <InfoField title={moment(date).format("MMMM Do YYYY, h:mm:ss a")}>
-          <span>Fecha</span> {formattedDate}
+          <span>
+            <AiOutlineCalendar />
+            Fecha
+          </span>{" "}
+          {formattedDate}
         </InfoField>
         <InfoField>
-          <span>Estado</span> Realizada
+          <span>
+            <AiOutlineCheckCircle />
+            Estado
+          </span>{" "}
+          Realizada
         </InfoField>
       </TransactionInfo>
+      <TransactionRemove onClick={remove}>
+        <AiOutlineDelete />
+        Borrar transacción
+      </TransactionRemove>
     </TransactionContainer>
   );
 };
